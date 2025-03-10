0aa1235679b81198292608a7fd25d580e11d5719
Gi0ia <gfrolik@student.tgm.ac.at>
2025-03-10 19:39:39 +0100

    create init for modules folder

---
+++ modules/__init__.py
@@ -0,0 +1,13 @@
+from .slack_module import SlackModule
+from .teams_module import TeamsModule
+from .email_module import EmailModule
+from .openai_module import OpenAIModule
+from .scheduler_module import SchedulerModule
+
+__all__ = [
+    "SlackModule",
+    "TeamsModule",
+    "EmailModule",
+    "OpenAIModule",
+    "SchedulerModule"
+]
